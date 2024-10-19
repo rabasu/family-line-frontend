6eb245697c2c022e7c83f4416cd8319112c8f33e
rabasu <149854533+rabasu@users.noreply.github.com>
2024-10-19 14:55:52 +0900

    アイコン類（つくりかけ）

---
+++ components/grade_icons/index.tsx
@@ -0,0 +1,56 @@
+import {
+  Mail,
+  Github,
+  Facebook,
+  Youtube,
+  Linkedin,
+  Twitter,
+  X,
+  Mastodon,
+  Threads,
+  Instagram,
+} from './icons'
+import { GradeCode } from '@/types/Grade'
+
+// 中央G1, 中央G2以下, 障害G1, 障害G2以下, 地方G1, 地方G2以下, 地方独自重賞
+const components = {
+  mail: Mail,
+  github: Github,
+  facebook: Facebook,
+  youtube: Youtube,
+  linkedin: Linkedin,
+  twitter: Twitter,
+  x: X,
+  mastodon: Mastodon,
+  threads: Threads,
+  instagram: Instagram,
+}
+
+type SocialIconProps = {
+  kind: keyof typeof components
+  href: string | undefined
+  size?: number
+}
+
+const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
+  if (!href || (kind === 'mail' && !/^mailto:\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(href)))
+    return null
+
+  const SocialSvg = components[kind]
+
+  return (
+    <a
+      className="text-sm text-gray-500 transition hover:text-gray-600"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+    >
+      <span className="sr-only">{kind}</span>
+      <SocialSvg
+        className={`fill-current text-gray-700 hover:text-primary-500 dark:text-gray-200 dark:hover:text-primary-400 h-${size} w-${size}`}
+      />
+    </a>
+  )
+}
+
+export default SocialIcon
