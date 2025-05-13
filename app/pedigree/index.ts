8e9e67f99107d9daa42fd65505b7972f8282b84d
rabasu <rabasu222@gmail.com>
2025-05-14 01:53:57 +0900

    アイリツシユアイズ系追加

--- app/pedigree/index.ts
+++ app/pedigree/index.ts
@@ -1,16 +1,17 @@
-import { Horse } from "@/types/Horse"
+import { Horse } from '@/types/Horse'
 
-import ROLL_YOUR_OWN from "./RollYourOwn"
-import FLORRIES_CUP from "./FlorriesCup";
+import ROLL_YOUR_OWN from './RollYourOwn'
+import FLORRIES_CUP from './FlorriesCup'
+import IRISH_EYES from './IrishEyes'
 
 type PedigreeList = {
   [key: string]: Horse
 }
 
-
 const pedigreeList = new Map<string, Horse>([
   ['roll_your_own', ROLL_YOUR_OWN],
   ['florries_cup', FLORRIES_CUP],
-]);
+  ['irish_eyes', IRISH_EYES],
+])
 
 export default pedigreeList
